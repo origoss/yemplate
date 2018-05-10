ccdbdc0d8ecbba5daf3a8fa95bb2bd8e6bb3f028
Gergely Szabo <gergely.szabo@origoss.com>
2018-05-10 13:53:10 +0200

    Commented out code has been removed

--- main.go
+++ main.go
@@ -6,17 +6,4 @@ import (
 
 func main() {
 	cmd.Execute()
-	// 	flag.Parse()
-	// 	var parameters, templated io.Reader
-	// 	var err error
-	// 	if parameters, err = os.Open(*parameterFile); err != nil {
-	// 		panic(err)
-	// 	}
-	// 	if templated, err = os.Open(*templatedFile); err != nil {
-	// 		panic(err)
-	// 	}
-
-	// 	if err := doTemplate(parameters, templated, os.Stdout); err != nil {
-	// 		panic(err)
-	// 	}
 }
